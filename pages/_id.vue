f8774b335868452ac86a8de3475888e4853a70f2
Parker McCrary <parker.mccrary@journeygroup.com>
2022-05-07 22:11:20 -0400

    feat(pages): create dynamic routes home pages

---
+++ pages/_id.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{ home.title }}</div>
+</template>
+
+<script>
+import homes from '~/data/homes'
+
+export default {
+  data() {
+    return {
+      home: null
+    }
+  },
+  created() {
+    const home = homes.find((home) => home.objectID === this.$route.params.id)
+    this.home = home
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
