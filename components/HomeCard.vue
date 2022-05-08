f1cf4260954e50596748254de7debdb51bb95e8b
Parker McCrary <parker.mccrary@journeygroup.com>
2022-05-07 21:59:07 -0400

    feat(home): create initial homepage

---
+++ components/HomeCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <img v-if="home.images && home.images.length > 0" :src="home.images[0]" />
+    <p>{{ home.title }}</p>
+    <p>
+      {{ home.location.address }} {{ home.location.city }}
+      {{ home.location.state }}
+    </p>
+    <p>{{ home.guests }} guests</p>
+    <p>{{ home.pricePerNight }} / night</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    home: { type: Object, required: true }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.home {
+  float: left;
+  margin: 10px;
+}
+
+img {
+  width: 200px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
 <template>
-  <Tutorial />
+  <div>
+    <div v-for="home in homes" :key="home.objectID" class="home">
+      <HomeCard :home="home" />
+    </div>
+  </div>
 </template>
 
 <script>
+import homes from '~/data/homes'
+
 export default {
-  name: 'IndexPage',
+  name: 'NuxtbnbHome',
+  data() {
+    return {
+      homes
+    }
+  }
 }
 </script>
