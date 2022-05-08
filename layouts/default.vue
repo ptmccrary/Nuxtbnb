ec9767404a46b026c6c440c8b0060a54119b08b1
Parker McCrary <parker.mccrary@journeygroup.com>
2022-05-07 23:58:39 -0400

    feat(layout): create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <header>
+      <NuxtLink to="/">Home</NuxtLink>
+    </header>
+
+    <main>
+      <Nuxt />
+    </main>
+
+    <footer></footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutDefault'
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+  background-color: #ccc;
+}
+</style>
