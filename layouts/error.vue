eeef82d2ccaa54435838c7f3c471714bf186719f
Parker McCrary <parker.mccrary@journeygroup.com>
2022-05-09 00:02:35 -0400

    feat(error): create error page

---
+++ layouts/error.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <img src="https://media1.tenor.com/images/99f00b32545bf5b5db8bf8ecbb7f0aec/tenor.gif?itemid=7971019"/><br/><br/>
+    <h1>Error!</h1>
+    {{ error.message }}
+    {{ error.statusCode }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutError',
+  props: {
+    error: { type: Object, required: true }
+  },
+  head() {
+    return {
+      title: 'WeeWoo Error!!'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+  text-align: center;
+}
+</style>
